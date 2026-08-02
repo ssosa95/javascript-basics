54dfeb5222f26428d619fa461c24c9be85b055ad
ssosa95 <ssosa95@hotmail.com>
2026-08-02 22:52:02 +0200

    Day 43: let and const

---
+++ script.js
@@ -0,0 +1,15 @@
+let age = 31;
+let weight = 99;
+const name = "Samuel";
+const race = "white";
+const nationality = "Italian";
+
+const scores = [10, 20, 30];
+scores.push(40);       // does this throw an error, or work fine?
+console.log(scores);
+
+console.log(age);
+console.log(weight);
+console.log(name);
+console.log(race);
+console.log(nationality);
