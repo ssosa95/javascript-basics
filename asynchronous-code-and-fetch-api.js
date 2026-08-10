7028b480a037598ca10c221a3969e9dc682127fc
ssosa95 <ssosa95@hotmail.com>
2026-08-10 19:50:10 +0200

    Day 50: await and async, cleaner fetch API code

--- asynchronous-code-and-fetch-api.js
+++ asynchronous-code-and-fetch-api.js
@@ -1,13 +1,14 @@
-fetch("https://jsonplaceholder.typicode.com/users/6")
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data);
-        console.log(data.name);
-        console.log(data.email);
-        console.log(data.address);
-        console.log(data.username);
-        console.log(data.address.street);
-        console.log(data.company.name);
-        console.log(data.company.catchPhrase);
-    });
+async function getData() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/9");
+    const data = await response.json();
+    console.log(data);
+    console.log(data.name);
+    console.log(data.email);
+    console.log(data.address);
+    console.log(data.username);
+    console.log(data.address.street);
+    console.log(data.company.name);
+    console.log(data.company.catchPhrase);
+}
 
+getData();
