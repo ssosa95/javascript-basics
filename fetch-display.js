ff0f79ee6cea8359da56cea816c0cf06bde45557
ssosa95 <ssosa95@hotmail.com>
2026-08-11 23:18:29 +0200

    Day 52: Parse and display JSON data in the DOM

---
+++ fetch-display.js
@@ -0,0 +1,17 @@
+async function displayAllUsers() {
+    const userList = document.querySelector("#userList");
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+    const data = await response.json();
+
+    data.forEach((user) => {
+        userList.innerHTML += `
+            <h2>${user.name}</h2>
+            <p>Address: ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
+            <p>Email: ${user.email}</p>
+            <p>Phone Number: ${user.phone}</p>
+            <p>Company: ${user.company.name}</p>
+        `;
+    });
+}
+
+displayAllUsers();
