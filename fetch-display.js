fba8c07424ce6058a5de4ca672a4c5572d64c6cf
ssosa95 <ssosa95@hotmail.com>
2026-08-13 21:44:32 +0200

    Day 53: Error handling

--- fetch-display.js
+++ fetch-display.js
@@ -1,17 +1,25 @@
 async function displayAllUsers() {
-    const userList = document.querySelector("#userList");
-    const response = await fetch("https://jsonplaceholder.typicode.com/users/");
-    const data = await response.json();
+    try {
+        const userList = document.querySelector("#userList");
+        const response = await fetch("https://jsonplacehilder.typicode.com/users/");
+        
+        if (!response.ok) {
+            throw new Error(`Request failed with status ${response.status}`);
+}
+        const data = await response.json();
 
-    data.forEach((user) => {
-        userList.innerHTML += `
-            <h2>${user.name}</h2>
-            <p>Address: ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
-            <p>Email: ${user.email}</p>
-            <p>Phone Number: ${user.phone}</p>
-            <p>Company: ${user.company.name}</p>
-        `;
-    });
+        data.forEach((user) => {
+            userList.innerHTML += `
+                <h2>${user.name}</h2>
+                <p>Address: ${user.address.street}, ${user.address.suite}, ${user.address.city}, ${user.address.zipcode}</p>
+                <p>Email: ${user.email}</p>
+                <p>Phone Number: ${user.phone}</p>
+                <p>Company: ${user.company.name}</p>
+            `;
+        });
+}   catch (error) {
+        console.log("Error fetching user data:", error);
+}
 }
 
 displayAllUsers();
